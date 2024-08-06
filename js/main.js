922b55b4bf7267424993a00a741a0ca5b7c3f5f4
Pedro mendonça junior <pedrojunior2112@gmail.com>
2024-08-05 21:07:06 -0400

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+        }
+    })
+})
